659f7c943ce7ce0eac7721470f6fa1ad49b12e1f
noellehat <noelleabunda1@gmail.com>
2022-11-28 15:39:15 +0800

    added base functions

---
+++ code.js
@@ -0,0 +1,9 @@
+//function that will randomly return 'rock' 'paper' or 'scissors'
+function getComputerChoice() {
+    
+}
+
+//function that plays a single round of rock paper scissors, takes two parameters
+function playGame(playerSelection, computerSelection) {
+    
+}
