3513a6daf90b48322f470d70cd2da563f91b8a01
noellehat <noelleabunda1@gmail.com>
2022-12-06 08:41:58 +0800

    Added functions for player and computer choice, as well as playing a round

--- code.js
+++ code.js
@@ -1,9 +1,32 @@
+let choices = ["rock", "paper", "scissors"];
+// TODO: loop this 5 times and determine a winner
+playGame ()
+
+
 //function that will randomly return 'rock' 'paper' or 'scissors'
-function getComputerChoice() {
-    
+function getComputerChoice(choices) {
+    //let computerSelection = choices[Math.floor(Math.random()*choices.length)];
+    let computerSelection = choices[0];
+    console.log("The computer chose: " + computerSelection);
+    return computerSelection;
+}
+
+function getPlayerChoice() {
+    //let playerChoice = prompt("choose rock, paper, or scissors");
+    let playerChoice = choices[0];
+    playerChoice = playerChoice.toLowerCase();
+    console.log("The player chose: " + playerChoice)
+    return playerChoice
 }
 
 //function that plays a single round of rock paper scissors, takes two parameters
-function playGame(playerSelection, computerSelection) {
-    
+function playGame() {
+    console.log("Play da game hit!")
+    let compSelection = getComputerChoice(choices);
+
+    let plrSelection = getPlayerChoice();
+        if (plrSelection == compSelection) {
+            console.log("Draw! Try again.")
+        } else console.log("okay")
+        // TODO: see who won the game
 }
