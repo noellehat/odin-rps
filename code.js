76c265f6441548fd6fd4fdb8f427b72e1cd02410
noellehat <noelleabunda1@gmail.com>
2022-12-06 11:42:56 +0800

    Changed conditions to nesting if else statements, now works

--- code.js
+++ code.js
@@ -1,19 +1,19 @@
 let choices = ["rock", "paper", "scissors"];
 // TODO: loop this 5 times and determine a winner
-playGame ()
-
+//playGame ()
+console.log(playGame())
 
 //function that will randomly return 'rock' 'paper' or 'scissors'
 function getComputerChoice(choices) {
-    //let computerSelection = choices[Math.floor(Math.random()*choices.length)];
-    let computerSelection = choices[1];
+    let computerSelection = choices[Math.floor(Math.random()*choices.length)];
+    //let computerSelection = choices[0];
     console.log("The computer chose: " + computerSelection);
     return computerSelection;
 }
 
 function getPlayerChoice() {
-    //let playerChoice = prompt("choose rock, paper, or scissors");
-    let playerChoice = choices[0];
+    let playerChoice = prompt("choose rock, paper, or scissors");
+    //let playerChoice = choices[0];
     playerChoice = playerChoice.toLowerCase();
     console.log("The player chose: " + playerChoice)
     return playerChoice
@@ -27,18 +27,26 @@ function playGame() {
     let plrSelection = getPlayerChoice();
         if (plrSelection == compSelection) {
             console.log("Draw! Try again.");
-        } else if (compSelection[0] && (plrSelection = "scissors")) {
-            console.log("Rock beats scissors! Computer wins!");
-        } else if (compSelection[1] && (plrSelection = "rock")) {
-            console.log("Paper covers rock! Computer wins!");
-        } else if (compSelection[2] && (plrSelection = "paper")) {
-            console.log("Scissors cuts paper! Computer wins!");
-        } else if ((plrSelection = "rock") && compSelection[2]) {
-            console.log("Rock beats scissors! Player wins!");
-        } else if ((plrSelection = "paper") && compSelection[0]) {
-            console.log("Paper covers rock! Player wins!");
-        } else if ((plrSelection = "scissors") && compSelection[1]) {
-            console.log("Scissors cuts paper! Player wins!");
-        } else console.log("You mispelled it dingus.");
+        } else if (plrSelection == "rock") {
+            if (compSelection == "scissors") {
+                return "Rock beats scissors! Player wins!"
+            } else {
+                return "Paper covers rock! Computer wins!"
+            }
+        } else if (plrSelection == "paper") {
+            if (compSelection == "rock") {
+                return "Paper covers rock! Player wins!"
+            } else {
+                return "Scissors beats paper! Computer wins!"
+            }
+        } else if (plrSelection == "scissors") {
+            if (compSelection == "paper") {
+                return "Scissors cuts paper! Player wins!"
+            } else {
+                return "Rock beats scissors! Computer wins!"
+            }
+        } else {
+            return "You typed something funny, dingus."
+        }
         // TODO: see who won the game
 }
